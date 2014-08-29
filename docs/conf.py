21020bb3769dbb652cad53f4e9b8bd78b2c42ad8
Andreas Sorge <as@asorge.de>
2014-08-29 23:04:08 +0200

    Docs: Configure intersphinx to link to python 3

--- docs/conf.py
+++ docs/conf.py
@@ -287,7 +287,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/3', None)}
 
 # custom style for sphinxcontrib-bibtex
 # http://sphinxcontrib-bibtex.readthedocs.org/en/latest/usage.html#custom-formatting-sorting-and-labelling
