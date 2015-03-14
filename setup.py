7e2f234a80ceb6abde44fdd6e47bb0a86fa9bf49
Andreas Sorge <as@asorge.de>
2015-03-14 12:12:13 +0100

    Reconfigure setup

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-import ez_setup
-ez_setup.use_setuptools()
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from codecs import open  # To use a consistent encoding
 import os
+import inspect
 import os.path
 
 import version
 
-here = os.path.abspath(os.path.dirname(__file__))
+__location__ = os.path.join(os.getcwd(), os.path.dirname(
+    inspect.getfile(inspect.currentframe())))
+here = __location__
 
 
 def get_version(*file_paths):
@@ -23,60 +25,59 @@ def get_version(*file_paths):
         version.write_version_file(ver, here, *file_paths)
     return ver
 
-# Get the long description from the relevant file
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
-setup(
-    name='fss',
-    version=get_version('fss', '_version.py'),
-    description=('Finite-size scaling analysis at phase transitions'),
-    long_description=long_description,
-    url='',
-    author='Andreas Sorge',
-    author_email='as@asorge.de',
-    license='ASL',
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Physics',
-    ],
-    keywords='',
-    packages=find_packages(exclude=['docs', 'tests*']),
-    install_requires=[
-        'numpy>=1.8',
-        'future>=0.13'
-    ],
-    include_package_data=True,  # include everything in source control
+def get_install_requirements(path):
+    content = open(os.path.join(__location__, path)).read()
+    return [req for req in content.split("\\n") if req != '']
 
-    # but exclude these files
-    exclude_package_data={'': [
-        '.gitignore',
-    ]},
-    package_data={
-        'fss': [],
-    },
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
+def setup_package():
+    # Get the long description from the relevant file
+    with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
 
-    setup_requires=['setuptools_git >= 1.1', ],
-    extras_require={
-        'doc': [
-            "sphinx>=1.2.2",
-            "sphinx_rtd_theme>=0.1.6",
-            "sphinxcontrib-bibtex>=0.3.1",
-            "numpydoc>=0.5",
+    setup(
+        name='fss',
+        version=get_version('fss', '_version.py'),
+        description=('Finite-size scaling analysis at phase transitions'),
+        long_description=long_description,
+        url='',
+        author='Andreas Sorge',
+        author_email='as@asorge.de',
+        license='ASL',
+        # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Scientific/Engineering :: Physics',
         ],
-    },
-    test_suite="tests"
-)
+        keywords='',
+        packages=find_packages(exclude=['docs', 'tests*']),
+        install_requires=get_install_requirements('requirements.txt') + [
+            'setuptools_git'
+        ],
+        include_package_data=True,  # include everything in source control
+
+        # but exclude these files
+        exclude_package_data={'': [
+            '.gitignore',
+        ]},
+        package_data={
+            'fss': [],
+        },
+
+        setup_requires=['setuptools_git >= 1.1', ],
+        extras_require={
+            'doc': get_install_requirements('requirements-doc.txt'),
+        },
+        test_suite="tests"
+    )
+
+
+if __name__ == "__main__":
+        setup_package()
