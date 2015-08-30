e41f498abdf74da52a46f594f11fecf99bb0bd46
Andreas Sorge <as@asorge.de>
2015-08-30 16:42:08 +0200

    ENH Refactor minimize_neldermead
    This basically mimics PR https://github.com/scipy/scipy/pull/5205
    
    Also effectively resets the nondeltaz value (closing #12)

--- fssa/fssa.py
+++ fssa/fssa.py
@@ -43,8 +43,8 @@ from collections import namedtuple
 import numpy as np
 import numpy.ma as ma
 import scipy.optimize
-from scipy.optimize.optimize import (OptimizeResult, _status_message,
-                                     wrap_function)
+
+from .optimize import _minimize_neldermead
 
 
 class ScaledData(namedtuple('ScaledData', ['x', 'y', 'dy'])):
@@ -504,197 +504,6 @@ def _neldermead_errors(sim, fsim, fun):
     return np.sqrt(np.diag(varco)), varco
 
 
-def _minimize_neldermead_witherrors(
-    fun, x0, args=(), callback=None,
-    xtol=1e-4, ftol=1e-4, maxiter=None, maxfev=None,
-    disp=False, return_all=False, with_errors=True,
-    **unknown_options
-):
-    """
-    Minimization of scalar function of one or more variables using the
-    Nelder-Mead algorithm.
-
-    Options for the Nelder-Mead algorithm are:
-        disp : bool
-            Set to True to print convergence messages.
-        xtol : float
-            Relative error in solution `xopt` acceptable for convergence.
-        ftol : float
-            Relative error in ``fun(xopt)`` acceptable for convergence.
-        maxiter : int
-            Maximum number of iterations to perform.
-        maxfev : int
-            Maximum number of function evaluations to make.
-
-    This function is called by the `minimize` function with
-    `method=minimize_neldermead_with_errors`. It is not supposed to be called
-    directly.
-
-    Notes
-    -----
-    Adapted from the original
-    :py:func:`scipy.optimize.optimize._minimize_neldermead` function to include
-    errors according to [7]_
-    Note that the errors are calculated for minimizing the reduced chi-square
-    statistic!
-
-    ******NOTICE***************
-    optimize.py module by Travis E. Oliphant
-
-    You may copy and use this module as you see fit with no
-    guarantee implied provided you keep this notice in all copies.
-    *****END NOTICE************
-
-    References
-    ----------
-    .. [7] J. A. Nelder and R. Mead, The Computer Journal 7, 308 (1965),
-       `doi:10.1093/comjnl/7.4.308 <http://dx.doi.org/10.1093/comjnl/7.4.308>`_
-
-    """
-
-    maxfun = maxfev
-    retall = return_all
-
-    fcalls, func = wrap_function(fun, args)
-    x0 = np.asfarray(x0).flatten()
-    N = len(x0)
-    rank = len(x0.shape)
-    if not -1 < rank < 2:
-        raise ValueError("Initial guess must be a scalar or rank-1 sequence.")
-    if maxiter is None:
-        maxiter = N * 200
-    if maxfun is None:
-        maxfun = N * 200
-
-    rho = 1
-    chi = 2
-    psi = 0.5
-    sigma = 0.5
-    one2np1 = list(range(1, N + 1))
-
-    if rank == 0:
-        sim = np.zeros((N + 1,), dtype=x0.dtype)
-    else:
-        sim = np.zeros((N + 1, N), dtype=x0.dtype)
-    fsim = np.zeros((N + 1,), float)
-    sim[0] = x0
-    if retall:
-        allvecs = [sim[0]]
-    fsim[0] = func(x0)
-    nonzdelt = 0.01
-    zdelt = 0.00025
-    for k in range(0, N):
-        y = np.array(x0, copy=True)
-        if y[k] != 0:
-            y[k] = (1 + nonzdelt) * y[k]
-        else:
-            y[k] = zdelt
-
-        sim[k + 1] = y
-        f = func(y)
-        fsim[k + 1] = f
-
-    ind = np.argsort(fsim)
-    fsim = np.take(fsim, ind, 0)
-    # sort so sim[0,:] has the lowest function value
-    sim = np.take(sim, ind, 0)
-
-    iterations = 1
-
-    while (fcalls[0] < maxfun and iterations < maxiter):
-        if (np.max(np.ravel(np.abs(sim[1:] - sim[0]))) <= xtol and
-                np.max(np.abs(fsim[0] - fsim[1:])) <= ftol):
-            break
-
-        xbar = np.add.reduce(sim[:-1], 0) / N
-        xr = (1 + rho) * xbar - rho * sim[-1]
-        fxr = func(xr)
-        doshrink = 0
-
-        if fxr < fsim[0]:
-            xe = (1 + rho * chi) * xbar - rho * chi * sim[-1]
-            fxe = func(xe)
-
-            if fxe < fxr:
-                sim[-1] = xe
-                fsim[-1] = fxe
-            else:
-                sim[-1] = xr
-                fsim[-1] = fxr
-        else:  # fsim[0] <= fxr
-            if fxr < fsim[-2]:
-                sim[-1] = xr
-                fsim[-1] = fxr
-            else:  # fxr >= fsim[-2]
-                # Perform contraction
-                if fxr < fsim[-1]:
-                    xc = (1 + psi * rho) * xbar - psi * rho * sim[-1]
-                    fxc = func(xc)
-
-                    if fxc <= fxr:
-                        sim[-1] = xc
-                        fsim[-1] = fxc
-                    else:
-                        doshrink = 1
-                else:
-                    # Perform an inside contraction
-                    xcc = (1 - psi) * xbar + psi * sim[-1]
-                    fxcc = func(xcc)
-
-                    if fxcc < fsim[-1]:
-                        sim[-1] = xcc
-                        fsim[-1] = fxcc
-                    else:
-                        doshrink = 1
-
-                if doshrink:
-                    for j in one2np1:
-                        sim[j] = sim[0] + sigma * (sim[j] - sim[0])
-                        fsim[j] = func(sim[j])
-
-        ind = np.argsort(fsim)
-        sim = np.take(sim, ind, 0)
-        fsim = np.take(fsim, ind, 0)
-        if callback is not None:
-            callback(sim[0])
-        iterations += 1
-        if retall:
-            allvecs.append(sim[0])
-
-    x = sim[0]
-    fval = np.min(fsim)
-    warnflag = 0
-    errors = None
-
-    if fcalls[0] >= maxfun:
-        warnflag = 1
-        msg = _status_message['maxfev']
-        if disp:
-            print('Warning: ' + msg)
-    elif iterations >= maxiter:
-        warnflag = 2
-        msg = _status_message['maxiter']
-        if disp:
-            print('Warning: ' + msg)
-    else:
-        msg = _status_message['success']
-        errors, varco = _neldermead_errors(sim, fsim, func)
-        if disp:
-            print(msg)
-            print("         Current function value: %f" % fval)
-            print("         Iterations: %d" % iterations)
-            print("         Function evaluations: %d" % fcalls[0])
-
-    result = OptimizeResult(fun=fval, nit=iterations, nfev=fcalls[0],
-                            status=warnflag, success=(warnflag == 0),
-                            message=msg, x=x, errors=errors, sim=sim,
-                            fsim=fsim, varco=varco)
-
-    if retall:
-        result['allvecs'] = allvecs
-    return result
-
-
 def autoscale(l, rho, a, da, rho_c0, nu0, zeta0, x_bounds=None, **kwargs):
     """
     Automatically scale finite-size data and fit critical point and exponents
@@ -801,13 +610,18 @@ def autoscale(l, rho, a, da, rho_c0, nu0, zeta0, x_bounds=None, **kwargs):
     ret = scipy.optimize.minimize(
         goal_function,
         [rho_c0, nu0, zeta0],
-        method=_minimize_neldermead_witherrors,
+        method=_minimize_neldermead,
         options={
             'xtol': 1e-2,
-            'ftol': 1e-2
+            'ftol': 1e-2,
+            'return_simplex': True,
         }
     )
 
+    errors, varco = _neldermead_errors(ret['sim'], ret['fsim'], goal_function)
+
+    ret['varco'] = varco
+    ret['errors'] = errors
     ret['rho'], ret['nu'], ret['zeta'] = ret['x']
     ret['drho'], ret['dnu'], ret['dzeta'] = ret['errors']
 
